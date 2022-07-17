21d003cb8722874125869c5adb6d835f9cb639dd
baejoonsoo <artns25@dsm.hs.kr>
2022-07-17 21:14:44 +0900

    chrome extension

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+  const imgs = document.querySelectorAll("img");
+
+  imgs.forEach((img) => {
+    img.src =
+      "https://images-ext-2.discordapp.net/external/_B4qBbeuje9uuBmdVvYNgO5-OGFhO0d-UNgL7uBu2kM/https/t1.daumcdn.net/cfile/tistory/9934214D5A5425792A";
+  });
+}, 500);
