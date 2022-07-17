dda5d439f6c38ca60cb90a377bedf75ba942cb73
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-07-17 21:52:03 +0900

    background-image로 들어간 사진도 변경

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ setInterval(() => {
     img.src =
       "https://images-ext-2.discordapp.net/external/_B4qBbeuje9uuBmdVvYNgO5-OGFhO0d-UNgL7uBu2kM/https/t1.daumcdn.net/cfile/tistory/9934214D5A5425792A";
   });
+
+  const allElem = document.querySelectorAll("body *");
+
+  allElem.forEach((el) => {
+    const checkBgImage =
+      getComputedStyle(el).getPropertyValue("background-image") !== "none";
+    if (checkBgImage) {
+      el.style.backgroundImage =
+        "url(https://images-ext-2.discordapp.net/external/_B4qBbeuje9uuBmdVvYNgO5-OGFhO0d-UNgL7uBu2kM/https/t1.daumcdn.net/cfile/tistory/9934214D5A5425792A)";
+    }
+  });
 }, 500);
